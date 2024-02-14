aa8d95a72468a2dd21799996840ee3ad222d13e2
Anastasia Kalko <a.kalko.work@mail.ru>
2024-02-14 22:14:14 +0300

    Задачи выполнены в полном объеме

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,16 +1,4 @@
 function factorial(n) {
-<<<<<<< HEAD
- let result = 1;
- if(n === 0 || n === 1) {
-  return result;
- }
-
- for (let i = 1; i <= n; i++) {
-    result = result * i
- }
-
- return result
-=======
   let result = 1;
   if (n === 0 || n === 1) {
     return result;
@@ -21,5 +9,4 @@ function factorial(n) {
   }
 
   return result;
->>>>>>> 68b7fc06468cbb73a99ce065e82e5d42114fbfd3
 }
